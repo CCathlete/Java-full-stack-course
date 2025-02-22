6e7a91bf4a7f4412fd2cfdd60f8520399f90668b
ccat <ccathlete01@gmail.com>
2025-02-22 07:35:59 +0100

    Reinstalled create-react-app.

--- react-workspace/react-basics/src/components/Welcome.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-function Welcome(name) {
-    return (0, jsx_runtime_1.jsxs)("h1", { children: ["Welcome, ", name, " ! "] });
-}
-exports.default = Welcome;
--- react-workspace/react-basics/src/components/Welcome.tsx
+++
@@ -1,5 +0,0 @@
-function Welcome(name: string) {
-    return <h1>Welcome, {name} ! </h1>
-}
-
-export default Welcome;
