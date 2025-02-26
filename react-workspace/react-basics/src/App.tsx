3ec5cba0dc5f34e9cc901e5dfe5848a134875f7f
ccat <ccathlete01@gmail.com>
2025-02-26 14:23:36 +0100

    Fixed the way the json.stringify call looks in User.

--- react-workspace/react-basics/src/App.tsx
+++ react-workspace/react-basics/src/App.tsx
@@ -5,6 +5,7 @@ import Welcome from './components/Welcome';
 import { Greeting } from './components/Greeting';
 import { Student, StudentProps } from './components/Student';
 import { Employee } from './components/Employee';
+import { User, UserProps } from './components/User';
 
 function App()
 {
@@ -12,6 +13,11 @@ function App()
     name: 'Bebe',
     age: 20,
     email: 'bebebebe@example.com',
+  };
+  const userData: Object = {
+    firstName: 'First',
+    lastName: 'Last',
+    email: 'Email',
   };
   return (
     <div className="App">
@@ -24,6 +30,7 @@ function App()
         <Employee
           { ...data } position='Developer'
         />
+        <User { ...userData as UserProps } />
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- react-workspace/react-basics/src/components/User.tsx
+++ react-workspace/react-basics/src/components/User.tsx
@@ -1,6 +1,6 @@
 import { useState, JSX } from "react";
 
-interface UserProps
+export interface UserProps
 {
   firstName: string;
   lastName: string;
@@ -11,15 +11,25 @@ export const User = ( props: UserProps ): JSX.Element =>
 {
   const [user, setUser]: [UserProps, CallableFunction] = useState( props );
 
+  const testData: UserProps = {
+    firstName: "First",
+    lastName: "Last",
+    email: "Email",
+  };
+
   const updateUser = (): void =>
   {
-    setUser( { firstName: "first" } );
+    setUser( testData );
   };
 
 
   return (
     <div>
-
+      <pre style={ { textAlign: "left" } }>
+        User:{ '\n' }
+        { JSON.stringify( user, null, 2 ) }
+      </pre>
+      <button onClick={ updateUser }>Update user</button>
     </div>
   );
 };
