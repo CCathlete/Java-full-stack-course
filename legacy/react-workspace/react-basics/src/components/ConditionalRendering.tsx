b6698088c9cfe2e95f68a0254d0a3f3b089c5cdc
ccat <ccathlete01@gmail.com>
2025-02-27 06:43:40 +0100

    Changed the directory structure to fit for a new project.

--- legacy/react-workspace/react-basics/src/components/ConditionalRendering.tsx
+++ legacy/react-workspace/react-basics/src/components/ConditionalRendering.tsx
@@ -32,7 +32,7 @@ export const ConditionalRendering = (): JSX.Element =>
       <h1>Conditional Rendering Example</h1>
       {/* <Component /> */ }
       {/* {
-        // A ternary operation, if isLoggedIn is true, then welcome 
+        // A ternary operation, if isLoggedIn is true, then welcome
         // user, else please log in.
         isLoggedIn ? <p>Hemlo user!</p> : <p>Please log in.</p>
       } */}
