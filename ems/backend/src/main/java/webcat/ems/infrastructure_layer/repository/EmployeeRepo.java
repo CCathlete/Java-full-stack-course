2f3b4471aff686e26583f7d2c667fb899e14115c
ccathlete <ccathlete01@gmail.com>
2025-11-25 16:28:18 +0100

    Cleaned the root of the project to start anew.

--- ems/backend/src/main/java/webcat/ems/infrastructure_layer/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package webcat.ems.infrastructure_layer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import webcat.ems.domain_layer.entities.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-
-}
--- legacy/springboot-rest-api/src/main/java/ccat/springboot/controller/exceptions/StudentNotDeletedException.java
+++
@@ -1,9 +0,0 @@
-package ccat.springboot.controller.exceptions;
-
-public class StudentNotDeletedException extends RuntimeException {
-
-    public StudentNotDeletedException(String funcName) {
-        super(funcName + ": " + "Error when deleting student.");
-    }
-
-}
